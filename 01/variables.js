489acaf9cde8ea9c25c8b2503aa20300b68c2dd6
ShinhyoBelok <shibo6937@gmail.com>
2023-06-30 23:15:53 -0400

    Add examples

--- 01/variables.js
+++ 01/variables.js
@@ -1,2 +1,8 @@
 var greetings = "Hello Shinhyo";
 console.log(greetings);
+var greetings = "Hello Shinhyo";
+console.log(greetings);var greetings = "Hello Shinhyo";
+console.log(greetings);var greetings = "Hello Shinhyo";
+console.log(greetings);var greetings = "Hello Shinhyo";
+console.log(greetings);var greetings = "Hello Shinhyo";
+console.log(greetings);
