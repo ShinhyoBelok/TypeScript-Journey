2cf251ab0de5a59c649f4fbeb8aac98b29e07c2f
ShinhyoBelok <shibo6937@gmail.com>
2023-07-10 17:17:26 -0400

    Enums

---
+++ enums.ts
@@ -0,0 +1,18 @@
+const AISELE = 0
+const MIDDLE = 1
+const WINDOW = 2
+
+const enum asientoOpcion {
+  PASILLO,
+  MEDIO,
+  VENTANA
+}
+
+const pasajero1 = asientoOpcion.PASILLO;
+
+const enum mesa {
+  pricipal = "pricipal",
+  ventana = 1,
+  ventana2,
+  ventana3
+}
