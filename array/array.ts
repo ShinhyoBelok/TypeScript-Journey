06bc6278cb970d69bcbaac5118eacd1c3dc4c2f7
ShinhyoBelok <shibo6937@gmail.com>
2023-07-10 17:18:07 -0400

    Array ts

---
+++ array/array.ts
@@ -0,0 +1,10 @@
+const superHero = [] //make it never type so you can't push data to array 
+
+const arrayElements: string[] = [];
+
+superHero.push("");
+arrayElements.push("");
+
+let arArElement: string[][] = [
+  ["klk", "dsfa", "asdfg"]
+]
